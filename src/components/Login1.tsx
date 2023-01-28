193d5e4db5d835ac05bf8e0d7f3487964ac6476d
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-01-28 18:35:58 +0530

    login in progress

--- src/components/Login1.tsx
+++ src/components/Login1.tsx
@@ -1,9 +1,32 @@
 import * as React from "react";
 import "../styles/login.css";
+import LoginIcon from "../assets/loginIcon.png";
+import GoogleIcon from "../assets/googleIcon.png";
+
 export default function Login1() {
 	return (
-		<main>
-			<h1>Login1</h1>
+		<main id="login">
+			<img src={LoginIcon} alt="Login Icon" />
+			<h1 id="motto">Write,Mention,Earn </h1>
+			<button id="googleLogin">
+				<img src={GoogleIcon} alt="Google Icon" />
+				Login with Google
+			</button>
+			<div id="OR-block">OR</div>
+			<div class="login-input-block">
+				<input
+					class="login-input"
+					type="text"
+					placeholder="Enter your email"
+				/>
+			</div>
+			<div class="login-input-block">
+				<input
+					class="login-input"
+					type="password"
+					placeholder="Enter your password"
+				/>
+			</div>
 		</main>
 	);
 }
